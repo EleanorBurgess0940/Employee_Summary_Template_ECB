f92f10c4145d2b1d59c87fd916e612d321e65030
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-04-29 12:16:01 -0400

    added questions about the manager

--- Develop/app.js
+++ Develop/app.js
@@ -1,16 +1,41 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+//const Manager = require("./lib/Manager");
+//const Engineer = require("./lib/Engineer");
+//const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+//const path = require("path");
+//const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+//const OUTPUT_DIR = path.resolve(__dirname, "output");
+//const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+//const render = require("./lib/htmlRenderer");
 
+function createManager() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "managerName",
+      message: "What is your Manager's Name?",
+    },
+    {
+      type: "input",
+      name: "managersID",
+      message: "What is your Manager's Id?",
+    },
+    {
+      type: "input",
+      name: "managersEmail",
+      message: "What is your Manager's email?",
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What is your Manager's Office number?",
+    },
+  ]);
+}
 
+createManager();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
